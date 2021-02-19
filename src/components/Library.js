194546c89b0dff181e06e88b6ce0e5f162fac4c7
Denin Grcic <deningrcic@MacBook-Pro-2.local>
2021-02-18 18:50:47 -0800

    Passed setState function to Library component

--- src/components/Library.js
+++ src/components/Library.js
@@ -2,13 +2,19 @@ import React from "react";
 import LibrarySong from "./LibrarySong";
 import { library } from "@fortawesome/fontawesome-svg-core";
 
-const Library = ({ songs }) => {
+const Library = ({ songs, setCurrentSong }) => {
   return (
     <div className="library">
       <h2>Library</h2>
       <div className="library-songs">
         {songs.map((song) => (
-          <LibrarySong song={song} />
+          <LibrarySong
+            songs={songs}
+            song={song}
+            id={song.id}
+            key={song.id}
+            setCurrentSong={setCurrentSong}
+          />
         ))}
       </div>
     </div>
