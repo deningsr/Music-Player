3e00cb38dce901b4f43de3981317f8a00298b221
Denin Grcic <deningrcic@MacBook-Pro-2.local>
2021-02-21 16:57:26 -0800

    Passed state to library to manage appearance

--- src/components/Library.js
+++ src/components/Library.js
@@ -2,9 +2,16 @@ import React from "react";
 import LibrarySong from "./LibrarySong";
 import { library } from "@fortawesome/fontawesome-svg-core";
 
-const Library = ({ songs, setCurrentSong, audioRef, isPlaying }) => {
+const Library = ({
+  songs,
+  setCurrentSong,
+  audioRef,
+  isPlaying,
+  setSongs,
+  libraryStatus,
+}) => {
   return (
-    <div className="library">
+    <div className={`library ${libraryStatus ? "active-library" : ""}`}>
       <h2>Library</h2>
       <div className="library-songs">
         {songs.map((song) => (
@@ -16,6 +23,7 @@ const Library = ({ songs, setCurrentSong, audioRef, isPlaying }) => {
             setCurrentSong={setCurrentSong}
             audioRef={audioRef}
             isPlaying={isPlaying}
+            setSongs={setSongs}
           />
         ))}
       </div>
