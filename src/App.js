badefdbcb1df48ea7af07da7e896469e7d460737
Denin Grcic <deningrcic@MacBook-Pro-2.local>
2021-02-22 23:28:32 -0800

    Customized input slider

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
   const [songInfo, setSongInfo] = useState({
     currentTime: null,
     duration: null,
+    animationPercentage: 0,
   });
 
   const [libraryStatus, setLibraryStatus] = useState(false);
@@ -25,7 +26,16 @@ function App() {
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
-    setSongInfo({ ...songInfo, currentTime: current, duration });
+    //Calculate Percentage
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const animation = Math.round((roundedCurrent / roundedDuration) * 100);
+    setSongInfo({
+      ...songInfo,
+      currentTime: current,
+      duration,
+      animationPercentage: animation,
+    });
   };
 
   return (
@@ -41,6 +51,7 @@ function App() {
         songInfo={songInfo}
         songs={songs}
         setCurrentSong={setCurrentSong}
+        setSongs={setSongs}
       />
       <Library
         songs={songs}
