7642c8221d7058a8075171ceb624274ee5915cc6
Denin Grcic <deningrcic@MacBook-Pro-2.local>
2021-02-23 19:44:29 -0800

    Refactoring

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { playAudio } from "../util";
 
 const LibrarySong = ({
   song,
@@ -10,8 +9,8 @@ const LibrarySong = ({
   isPlaying,
   setSongs,
 }) => {
-  const songSelectHandler = () => {
-    setCurrentSong(song);
+  const songSelectHandler = async () => {
+    await setCurrentSong(song);
     //Add active state
     const newSongs = songs.map((song) => {
       if (song.id === id) {
@@ -28,6 +27,7 @@ const LibrarySong = ({
     });
     setSongs(newSongs);
     //Check if song is playing
+    if (isPlaying) audioRef.current.play();
   };
 
   return (
